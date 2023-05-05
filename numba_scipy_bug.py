90aef4ecc41fe5ccc051623421b5bf5f71c19a44
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-05 09:50:59 +0200

    add disable_jit matrix; check python version

--- numba_scipy_bug.py
+++ numba_scipy_bug.py
@@ -14,5 +14,5 @@ y0 = np.ones(2)
 
 with warnings.catch_warnings(record=True):
     warnings.simplefilter("error")
-    for _ in range(100):
+    for _ in range(1000):
         scipy.integrate.solve_ivp(fun=f, t_span=(0, 1), y0=y0, method="BDF")
